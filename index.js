8c44038fe3e75e457446ac6c36c45c4e8ee2e493
syihwamoza <mozakastella26@gmail.com>
2025-10-15 12:01:13 +0700

    mengambil semua data biodata

--- index.js
+++ index.js
@@ -27,7 +27,13 @@ db.connect((err) => {
 app.get('/biodata', (req, res) => {
     const query = 'SELECT * FROM biodata';
     db.query(query, (err, results) => {
-        
+        if(err){
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(results);
+    });
+});
+
 
 
 // Root route
